7c7c75f1be439a501dc642f66151940565bbb346
Matt Redfearn <matt.redfearn@gmail.com>
2016-05-26 08:53:53 +0100

    Make weather parsing a bit more generic and more defined by the template.

--- server/programs/parse_weather.py
+++ server/programs/parse_weather.py
@@ -1,62 +1,50 @@
-import urllib3, urllib
+#!/usr/bin/env/python3
+import urllib.request, urllib.parse
 from xml.dom import minidom
 import json
 import datetime
 import codecs
 
 
-#Code of my city, if you don't know what to do here, read the README
+# Code of my city, if you don't know what to do here, read the README
 CODE = "39292"
+# Units should be 'c' or 'f'
+UNITS='c'
 
-http = urllib3.PoolManager()
-
-#weather = http.request('GET', 'http://weather.yahooapis.com/forecastrss?w=' + CODE + '&u=c')
 baseurl = "https://query.yahooapis.com/v1/public/yql?"
-yql_query = "q=select%20*%20from%20weather.forecast%20where%20u='c'%20and%20woeid=" + CODE
-#yql_query = "select wind from weather.forecast where woeid=" + CODE
-#yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
-yql_url = baseurl + yql_query +"&format=json"
+yql_query = urllib.parse.quote("select * from weather.forecast where u='" + UNITS + "' and woeid=" + CODE)
+yql_url = baseurl + "q=" + yql_query +"&format=json"
 
-weather = http.request('GET', yql_url)
+print(yql_url)
 
-#weather_xml = weather.data
-#print(weather_xml)
-#dom = minidom.parseString(weather_xml)
-#print(dom)
-#Get weather Tags
-#xml_temperatures = dom.getElementsByTagName('yweather:forecast')
-print(weather.data)
-reader = codecs.getreader("utf-8")
-data = json.loads(codecs.decode(weather.data, "utf-8"))
+weather = urllib.request.urlopen(yql_url)
+
+data = json.loads(codecs.decode(weather.read(), "utf-8"))
 print(data)
-xml_temperatures = data['query']['results']['channel']['item']['forecast']
-print(xml_temperatures)
-
-#Get today Tag
-today = xml_temperatures[0]
-
-#Get info
-low = today['low']
-high = today['high']
-image = today['code']
-date = today['date']
-image_url = 'icons/' + image + '.svg'
+forecast = data['query']['results']['channel']['item']['forecast']
+print(forecast)
 
 # Open SVG to process
 output = codecs.open('icons/template.svg', 'r', encoding='utf-8').read()
 
+print("Forecast:")
+print(forecast)
 
-#Read icon (Just the path line)
-f = codecs.open(image_url ,'r', encoding='utf-8')
-f.readline()
-icon = f.readline()
-f.close()
+for i in range(len(forecast)):
+	day = forecast[i]
+	print("Day:")
+	print(day)
 
-# Insert icons and temperatures
-#output = output.replace('TODAY',date)
-output = output.replace('ICON_ONE',icon)
-output = output.replace('HIGH_ONE',high)
-output = output.replace('LOW_ONE',low)
+	image_url = 'icons/' + day['code'] + '.svg'
+	# Read icon (Just the path line)
+	f = codecs.open(image_url ,'r', encoding='utf-8')
+	f.readline()
+	day['icon'] = f.readline()
+	f.close()
+
+	for k, v in day.items():
+		output = output.replace('DAY_%d_%s' % (i, k), v)
+
 
 # Write output
 codecs.open('after-weather.svg', 'w', encoding='utf-8').write(output)
