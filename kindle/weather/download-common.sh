7bbb279ba3be1308ddf48e97b79ed2d618c03931
Matt Redfearn <matt.redfearn@gmail.com>
2016-12-18 10:34:35 +0000

    Kindle: download-common: Retry up to 10 times to get the image
    If after 10 attempts it's still not working, set the error image and bail.
    
    Yahoo seems to have a bit of an issue where it sometimes returns no valid
    data, so we need a few retires to cope with that.
    
    Signed-off-by: Matt Redfearn <matt.redfearn@gmail.com>

--- kindle/weather/download-common.sh
+++ kindle/weather/download-common.sh
@@ -7,14 +7,25 @@ wancontrol wanon
 
 sleep 30
 
-if wget -O $OUT $SRC; then
-echo "OK"
-else
-	sleep 60
-	if wget -O $OUT $SRC; then
-        cp weather-image-error.png $OUT
-	fi
-fi
+a=0
+
+while true; do
+    if wget -O $OUT $SRC; then
+        echo "OK"
+        break
+    else
+        a=`expr $a + 1`
+        echo "Error downloading attempt $a"
+        if [ $a -eq 10 ]; then
+            echo "Setting error image"
+            cp weather-image-error.png $OUT
+            break
+        else
+            # Try again in 30 seconds
+            sleep 30
+        fi
+    fi
+done
 
 # Disable 3G connection
 wancontrol wanoff
